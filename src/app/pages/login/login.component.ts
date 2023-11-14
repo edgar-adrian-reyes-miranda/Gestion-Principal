7624d5da1f8c44793826c25f0dde738477b65091
EDGAR REYES <edgar09reyes@outlook.es>
2023-11-14 14:02:02 -0600

    ok

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -18,28 +18,60 @@ export class LoginComponent  implements OnInit{
     "username":"",
     "password":""
   }
+
  
 
-  constructor (private snack:MatSnackBar, private LoginService:LoginService){
+  constructor(private snack:MatSnackBar,private loginService:LoginService,private router:Router) { }
 
-  }
   ngOnInit():void{
 
   }
   formSubmit(){
-    if(this.loginData.username.trim()=='' || this.loginData.username.trim()== null){
-      this.snack.open('El nombre de usuario es requerido !!', 'Aceptar',{
-        duration: 3000
+    if(this.loginData.username.trim() == '' || this.loginData.username.trim() == null){
+      this.snack.open('El nombre de usuario es requerido !!','Aceptar',{
+        duration:3000
       })
       return;
-           }
-           if(this.loginData.password.trim()== ''|| this.loginData.password.trim()==null){
-            this.snack.open('La contraseña es requerdia: ', 'Aceptar',{
-                  duration: 3000
-            })
-            return;
-           }
+    }
 
+    if(this.loginData.password.trim() == '' || this.loginData.password.trim() == null){
+      this.snack.open('La contraseña es requerida !!','Aceptar',{
+        duration:3000
+      })
+      return;
+    }
+
+    this.loginService.generatetoken(this.loginData).subscribe(
+      (data:any) => {
+        console.log(data);
+        this.loginService.loginUser(data.token);
+        this.loginService.getCurrentUser().subscribe((user:any) => {
+          this.loginService.setUser(user);
+          console.log(user);
+
+          if(this.loginService.getUserRole() == 'ADMIN'){
+            //dashboard admin
+            //window.location.href = '/admin';
+            this.router.navigate(['admin']);
+            this.loginService.loginStatusSubjec.next(true);
+          }
+          else if(this.loginService.getUserRole() == 'NORMAL'){
+            //user dashboard
+            //window.location.href = '/user-dashboard';
+            this.router.navigate(['user-dashboard']);
+            this.loginService.loginStatusSubjec.next(true);
+          }
+          else{
+            this.loginService.logout();
+          }
+        })
+      },(error: any) => {
+        console.log(error);
+        this.snack.open('Detalles inválidos , vuelva a intentar !!','Aceptar',{
+          duration:3000
+        })
+      }
+    )
 
 }
 }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -6,6 +6,19 @@ import baseUrl from './helper';
   providedIn: 'root'
 })
 export class LoginService {
+  loginStatusSubjec: any;
+  getUserRole() {
+    throw new Error('Method not implemented.');
+  }
+  getCurrentUser() {
+    throw new Error('Method not implemented.');
+  }
+  setUser(user: any) {
+    throw new Error('Method not implemented.');
+  }
+  loginUser(token: any) {
+    throw new Error('Method not implemented.');
+  }
   añadirUsuario(user: { username: string; nombre: string; password: string; primer_apellido: string; segundo_apellido: string; email: string; }) {
     throw new Error('Method not implemented.');
   }
